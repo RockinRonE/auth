af86b1d216ba35feb1bd04bc07c131138a494232
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-09-05 08:29:18 -0500

    prettier formatted

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,43 +1,57 @@
-const passport = require('passport');
-const User = require('../models/user');
-const config = require('../config');
-const JwtStrategy = require('passport-jwt').Strategy; 
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const LocalStrategy = require('passport-local');
+const passport = require("passport");
+const User = require("../models/user");
+const config = require("../config");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
 
 // Create local strategy
-const localOptions = { usernameField: 'email' };
-const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
-    User.findOne({ email }, function(err, user){
-        if(err) { return done(err); }
-        if(!user) { return done(null, false); }
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOptions, function(
+  email,
+  password,
+  done
+) {
+  User.findOne({ email }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
 
-        user.comparePassword(password, function(err, isMatch) {
-            if(err) { return done(err); }
-            if(!isMatch) { return done(null, false); }
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) {
+        return done(err);
+      }
+      if (!isMatch) {
+        return done(null, false);
+      }
 
-            return done(null, user);
-        });
+      return done(null, user);
     });
+  });
 });
 
 // setup options for JWT strategy
 const jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-    secretOrKey: config.secret
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: config.secret
 };
 
 // Creat JWT strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
-    User.findById(payload.sub, function(err, user) {
-        if(err) { return done(err, false); }
+  User.findById(payload.sub, function(err, user) {
+    if (err) {
+      return done(err, false);
+    }
 
-        if(user) {
-            done(null, user);
-        } else {
-            done(null, false);
-        }
-    });
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
 });
 
 // Tell passport to use this strategy
